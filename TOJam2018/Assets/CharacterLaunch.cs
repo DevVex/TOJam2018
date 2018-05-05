91d44dab76e9d353ae91f5ac11f36199a4fe8f73
John Vieira <john.c.vieira@gmail.com>
2018-05-04 20:04:37 -0400

    Add - character and launch code

---
+++ TOJam2018/Assets/CharacterLaunch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterLaunch : MonoBehaviour {
+
+    public Rigidbody2D body;
+    public float force;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("space");
+
+            Launch();
+        }
+    }
+
+    void Launch()
+    {
+        Vector3 dir = new Vector3(150f, 50f, 0f);
+        dir.Normalize();
+        body.AddForce(dir * force);
+    }
+}
---
+++ TOJam2018/Assets/FollowCharacterX.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCharacterX : MonoBehaviour {
+
+    public GameObject player;       //Public variable to store a reference to the player game object
+
+    // LateUpdate is called after Update each frame
+    void Update()
+    {
+        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+        transform.position = new Vector3(player.transform.position.x , transform.position.y, player.transform.position.z);
+
+    }
+}
